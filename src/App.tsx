4d35a6b13640d0fa55e34c71ca46c1dcb3846fd1
lucap9056 <jim12366219@gmail.com>
2026-08-04 22:01:25 +0800

    chore(App,main): mount Toaster/ConfirmDialogProvider, drop old providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Alerts, { useAlerts } from "global-components/alerts";
 import Loader, { useLoader } from "global-components/loader";
-import Notifications, {
-  useNotifications,
-} from "global-components/notifications";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { buildResult, match } from "resultant.js/rustify";
+import { Toaster, toast } from "sonner";
 import "./i18n";
 
 import Images from "components/images";
@@ -19,28 +16,20 @@ import MainView from "components/viewer";
 import RoutePaths from "route-paths/index";
 import Google, { NotSignedInError } from "services/google";
 import Imgstor, { ImgstorProvider } from "services/imgstor";
-import { Message } from "structs/message";
 
 function App() {
-  const notifications = useNotifications();
   const loader = useLoader();
-  const alerts = useAlerts();
 
   const { t } = useTranslation();
   const [imgstor, SetImgstor] = useState<Imgstor>();
   const [loaded, setLoaded] = useState(false);
   const [authInstance, SetAuthInstance] = useState<gapi.auth2.GoogleAuth>();
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: intentionally runs once on mount to initialize Google/Imgstor; re-running on t/loader/notifications/alerts changes (e.g. language switch) would needlessly restart sign-in
+  // biome-ignore lint/correctness/useExhaustiveDependencies: intentionally runs once on mount to initialize Google/Imgstor; re-running on t/loader changes (e.g. language switch) would needlessly restart sign-in
   useEffect(() => {
     const loading = loader.append();
 
-    const loadingMessage = notifications.append(
-      new Message({
-        type: Message.Type.ALERT,
-        content: t("google.loading"),
-      }),
-    );
+    const loadingToastId = toast.loading(t("google.loading"));
 
     buildResult(async () => {
       const google = await Google.new();
@@ -56,19 +45,14 @@ function App() {
             SetImgstor(undefined);
             SetAuthInstance(err.authInstance);
           } else {
-            alerts.append(
-              new Message({
-                type: Message.Type.ERROR,
-                content: (err as Error).message,
-              }),
-            );
+            toast.error((err as Error).message);
           }
         },
       });
 
       setLoaded(true);
       loading.remove();
-      loadingMessage.remove();
+      toast.dismiss(loadingToastId);
     });
   }, []);
 
@@ -92,12 +76,7 @@ function App() {
         SetImgstor(value);
       },
       Err(err) {
-        notifications.append(
-          new Message({
-            type: Message.Type.ERROR,
-            content: t("google.signin.fail"),
-          }),
-        );
+        toast.error(t("google.signin.fail"));
         console.error(err);
       },
     });
@@ -135,9 +114,8 @@ function App() {
         )
       ) : null}
 
-      <Alerts />
       <Loader.Component />
-      <Notifications />
+      <Toaster />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import { AlertsProvider } from "global-components/alerts";
+import { ConfirmDialogProvider } from "global-components/confirm-dialog";
 import { LoaderProvider } from "global-components/loader";
-import { NotificationsProvider } from "global-components/notifications";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
@@ -15,10 +14,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <LoaderProvider>
-    <AlertsProvider>
-      <NotificationsProvider>
-        <App />
-      </NotificationsProvider>
-    </AlertsProvider>
+    <ConfirmDialogProvider>
+      <App />
+    </ConfirmDialogProvider>
   </LoaderProvider>,
 );
