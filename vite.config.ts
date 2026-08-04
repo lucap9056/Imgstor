dbb224b5b10802cbb004347410e30316baaa3864
lucap9056 <jim12366219@gmail.com>
2026-08-04 21:14:16 +0800

    style: sort imports in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from "@vitejs/plugin-react";
 import fs from "node:fs";
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import compression from "vite-plugin-compression";
 import { viteStaticCopy } from "vite-plugin-static-copy";
